59d948c3d55397fb494469fa514164c8627d1f18
caixw <xnotepad@gmail.com>
2015-01-21 20:38:14 +0800

    更新文档

--- doc.go
+++ doc.go
@@ -2,15 +2,13 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-// logs 是对标准库的log的一个简单扩展，定义了6个级别的日志：
-// ERROR,INFO,TRACE,DEBUG,CRITICAL,WARN，用户可以根据自己的需求，
-// 向指定级别的日志输出特定内容，也可以通过xml文件定义每个级别的日志行为。
+// 基于xml配置的日志系统。
 //
-// 默认情况下，所有日志都输出到ioutil.Discard。
-// 用户需要显示地调用相应的初始化函数对日志进行初始化。
+// logs 定义了6个级别的日志：ERROR,INFO,TRACE,DEBUG,CRITICAL,WARN。
+// 用户可以根据自己的需求，通过xml配置文件自定义每个日志输出行为，
+// 默认情况下，所有日志都输出到ioutil.Discard，即不展现任何内容。
 //
 // 以下是一个简短的xml配置文件范本，具体的可参考目录下的config.xml。
-//  xml:
 //  <?xml version="1.0" encoding="utf-8" ?>
 //  <logs>
 //      <debug>
@@ -25,7 +23,7 @@
 //      <!-- 除了debug和info，其它4个依然输出到ioutil.Discard -->
 //  </logs>
 //
-//  go:
+// 然后就可以调用go代码输出日志内容:
 //  logs.Debug(...)
 //  logs.Debugf("format", v...)
 //  logs.DEBUG.Println(...)
@@ -36,10 +34,11 @@
 //  // 向所有级别的日志输出内容。
 //  logs.All(...)
 //
-// 上面的配置文件表示DEBUG级别的内容输出前都进被buffer实例进行缓存，
-// 当量达到10条时，一次性向rotate和stmp输出。
+// 上面的配置文件表示DEBUG级别的内容输出前都进被缓存，当量达到10条时，
+// 一次性向rotate和stmp输出。
 // 其中buffer、rotate、stmp甚至是debug和info都是一个个实现io.Writer
 // 接口的结构。通过Register()注册成功之后，即可以使用。
+//
 //
 //
 // 配置文件：
@@ -58,25 +57,31 @@
 // 用户也可以自己向logs注册自己的实现。
 //
 // 1. buffer:
+//
 // 缓存工具，比如上面的示例中，所有向debug输出的内容，都会被buffer缓存，
 // 直到数量达到10条，buffer才会一起向rotate和stmp输出内容。
 // 只有一个size属性，用于指定缓存的数量。
 //
 // 2. rotate:
-// 这是一个按文件大写自动分割日志的实例，允许dir和size两个属性。
-// 其中dir表示的是日志存放的目录；size表示的是每个日志的大概大小，
+//
+// 这是一个按文件大写自动分割日志的实例，以第一条记录的产生时间作为文件名，
+// 允许prefix，dir和size三个属性。其中prefix表示日志文件的前缀，留空表示没有前缀；
+// dir表示的是日志存放的目录；size表示的是每个日志的大概大小，
 // 可以是数值(单位为Byte)或是5M,5G等这类字符串(支持k,m,g三个后缀，不区分大小写)。
 //
 // 3. stmp:
+//
 // 发送邮件的实例，可定义的属性为：username 发送邮件的账号；
 // password账号对应的密码；host stmp的主机；subject 邮件的主题；sendTo，
 // 接收人地址，多个收件地址使用分号分隔。
 //
 // 4. console:
+//
 // 向控制台输出内容。可定义的属性为：foreground，background和output，
 // 其中output只能为"stderr", "stdout"两个值，表示输出的具体方向，
 // 默认值为"stderr"；而foreground和background表示输出时的前景和背景色，
 // 其值在github.com/issue9/term/colors中定义。
+//
 //
 // 自定义
 //
